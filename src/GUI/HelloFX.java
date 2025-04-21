80b96280b9809c331367694f9a206449162eb59b
Piccio Hub <201469659+piccio-code@users.noreply.github.com>
2025-04-21 14:34:48 +0200

    GUI basic structure

---
+++ src/GUI/HelloFX.java
@@ -0,0 +1,30 @@
+package GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
+public class HelloFX extends Application {
+
+
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Design.fxml")));
+        Scene scene = new Scene(root);
+
+        stage.setScene(scene);
+        stage.show(); // to show the stage
+    }
+}
