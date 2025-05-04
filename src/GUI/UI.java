6229e5c702391e3496e72b931fb55e4022cbd448
Piccio Hub <201469659+Piccio-Code@users.noreply.github.com>
2025-05-04 16:31:16 +0200

    Check if the server is on before connecting to the server.

--- src/GUI/UI.java
+++ src/GUI/UI.java
@@ -22,43 +22,47 @@ public class UI extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader clientLoader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("Design.fxml")));
-        FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
-        String css = Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm();
+        try (Socket socket = new Socket("localHost", 4444)) {
+            FXMLLoader clientLoader = new FXMLLoader(Objects.requireNonNull(getClass().getResource("Design.fxml")));
+            FXMLLoader loginLoader = new FXMLLoader(getClass().getResource("Login.fxml"));
+            String css = Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm();
 
-        Parent loginRoot = loginLoader.load();
-        Scene login = new Scene(loginRoot, 1200, 720);
+            Parent loginRoot = loginLoader.load();
+            Scene login = new Scene(loginRoot, 1200, 720);
 
 
-        Parent clientRoot = clientLoader.load();
-        Scene client = new Scene(clientRoot, 1200, 720);
+            Parent clientRoot = clientLoader.load();
+            Scene client = new Scene(clientRoot, 1200, 720);
 
-        Controller controller = clientLoader.getController();
-        LoginController loginController = loginLoader.getController();
+            Controller controller = clientLoader.getController();
+            LoginController loginController = loginLoader.getController();
 
-        Socket socket = new Socket("localHost", 4444);
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-        controller.setConnection(socket, out);
-        Thread mT = new Thread(new Receiver(in, controller));
+            controller.setConnection(socket, out);
+            Thread mT = new Thread(new Receiver(in, controller));
 
-        loginController.controller = controller;
-        loginController.stage = stage;
-        loginController.scene = client;
-        loginController.out = out;
-        loginController.in = in;
-        loginController.thread = mT;
+            loginController.controller = controller;
+            loginController.stage = stage;
+            loginController.scene = client;
+            loginController.out = out;
+            loginController.in = in;
+            loginController.thread = mT;
 
-        client.getStylesheets().add(css);
-        login.getStylesheets().add(css);
+            client.getStylesheets().add(css);
+            login.getStylesheets().add(css);
 
-        stage.resizableProperty().set(false);
-        stage.titleProperty().set("Bro Chat");
-        stage.getIcons().add(new Image("GUI/Logo.jpeg"));
-        stage.setOnCloseRequest(_ -> out.println("CLOSE_CONNECTION"));
+            stage.resizableProperty().set(false);
+            stage.titleProperty().set("Bro Chat");
+            stage.getIcons().add(new Image("GUI/Logo.jpeg"));
+            stage.setOnCloseRequest(_ -> out.println("CLOSE_CONNECTION"));
 
-        stage.setScene(login);
-        stage.show();
+            stage.setScene(login);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println("ERROR:on connection to the server");
+            System.exit(-1);
+        }
     }
 }
